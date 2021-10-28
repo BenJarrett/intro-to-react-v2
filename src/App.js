e4d57d222277972e770754e3ebc49a0bbc9b39c4
BenJarrett <b.cl.jarrett@gmail.com>
2021-10-28 14:08:47 -0500

    Added to app.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,35 @@ import "./App.css";
 import Counter from "./Counter";
 
 
+const array = [
+  {
+    appName: "Counter 1",
+    initialValue: 200
+  },
+  {
+    appName: "Counter 6",
+    initialValue: 0
+  },
+  {
+    appName: "Counter 3",
+    initialValue: 100
+  },
+  {
+    appName: "Counter 15",
+    initialValue: 100
+  }
+];
+
 const App = () => {
+
+  const countersOnDom = () => {
+    return array.map((counter, i) => <Counter key={i} appName={counter.appName} initialValue={counter.initialValue}/>)
+  }
+
+
   return (
     <>
-    <Counter appName="Counter 1" counterValue={200}/>
-    <Counter appName="Counter 2" counterValue={200}/>
-    <Counter appName="Counter 3" counterValue={200}/>
+    {countersOnDom()}
     </>
   )
 };
